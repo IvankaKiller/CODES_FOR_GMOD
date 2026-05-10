14e9070df6ab6f7ccba7f9cbdbc2ff0e55124210
Ivanka_Da <sepilovivan89@gmail.com>
2026-05-10 22:45:05 +0700

    lfslf

--- lua/autorun/code.lua
+++ lua/autorun/code.lua
@@ -1,8 +1,13 @@
 local pyzo = 10
-
+local pisa = 65
+---------------------------
 local function popa(x)
 	x = x * 2
 	return x
 end
-
-print(popa(pyzo))
+---------------------------
+if pyzo + pisa < 100 then 
+	print("<- 100")
+else
+	print("-> 100")
+end
